be7804d161a5e66294f725a59ad3039b5afe4cc5
zerodie7 <zerodie.7@hotmail.com>
2016-09-22 12:45:02 -0500

    Practica 2
    Segunda parte, practica 2

--- P2_E3/GLApplication.cpp
+++ P2_E3/GLApplication.cpp
@@ -11,6 +11,16 @@ Shader shader;
 
 GLuint VAO, VBO, EBO;
 
+Sphere sp(1.5, 50, 50, MODEL_MODE::VERTEX_COLOR);
+Sphere sp2(1.5, 50, 50, MODEL_MODE::VERTEX_LIGHT_TEXTURE);
+Shader lightingShader;
+Shader lampShader;
+Texture textureDifuse(GL_TEXTURE_2D, "Textures/container2.png");
+Texture textureSpecular(GL_TEXTURE_2D, "Textures/container2_specular.png");
+
+
+
+
 GLApplication::GLApplication() :
 		windowManager(nullptr), camera(nullptr) {
 }
@@ -34,40 +44,113 @@ void GLApplication::initialize() {
 	glViewport(0, 0, WindowManager::screenWidth, WindowManager::screenHeight);
 	glClearColor(0.2f, 0.2f, 0.2f, 0.0f);
 
-	//glEnable(GL_CULL_FACE);
+	glEnable(GL_CULL_FACE);
 	glEnable(GL_DEPTH_TEST);
 
+	sp.init();
+	sp.load();
+
+	sp2.init();
+	sp2.load();
+
+	textureDifuse.load();
+	textureSpecular.load();
+
 	shader.initialize("Shaders/shader.vs", "Shaders/shader.fs");
+	lightingShader.initialize("Shaders/lightingSpecularMap.vs","Shaders/lightingSpecularMap.fs");
+	lampShader.initialize("Shaders/lampShader.vs", "Shaders/lampShader.fs");
+
 
 }
 void GLApplication::applicationLoop() {
 	bool processInput = true;
 
-	glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+	glm::vec3 lightPos( 1.0f, -2.5f, 1.0f);
 	Model objModel("objects/green_arrow/Green_Arrow_A.obj");
 	Model objModel1("objects/flash/Flash.obj");
 	Model objModel2("objects/death/Deathstroke.obj");
 
+	double lastTime = TimeManager::Instance().GetTime();
 
 	while (processInput) {
 		processInput = windowManager->processInput(true);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		shader.turnOn();
+
+		//Rotar esfera de luz
+		GLfloat timeValue = TimeManager::Instance().GetTime() - lastTime;
+		GLfloat valX = sin((GLfloat)timeValue * 1.0f) * 3.0f;
+		GLfloat valZ = cos((GLfloat)timeValue * 1.0f) * 3.0f;
+		lightPos.x = valX;
+		lightPos.z = valZ;
+
 
 		// Transformation matrices
 		glm::mat4 view = camera->GetViewMatrix();
 		glm::mat4 projection;
-		projection = glm::perspective(45.0f, (GLfloat) WindowManager::screenWidth / (GLfloat) WindowManager::screenHeight, 0.1f, 100.0f);
-		
+		projection = glm::perspective(45.0f, (GLfloat)WindowManager::screenWidth / (GLfloat)WindowManager::screenHeight, 0.1f, 100.0f);
+
+
 		// Get the uniform locations
-		GLint modelLoc = shader.getUniformLocation("model");
-		GLint viewLoc = shader.getUniformLocation("view");
-		GLint projLoc = shader.getUniformLocation("projection");
+		GLint modelLoc = lightingShader.getUniformLocation("model");
+		GLint viewLoc = lightingShader.getUniformLocation("view");
+		GLint projLoc = lightingShader.getUniformLocation("projection");
 
 		// Pass the matrices to the shader
 		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
 		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+		// Set material properties
+		GLint matDiffuseLoc = lightingShader.getUniformLocation("material.diffuse");
+		GLint matSpecularLoc = lightingShader.getUniformLocation("material.specular");
+		GLint matShineLoc = lightingShader.getUniformLocation("material.shininess");
+		glUniform1i(matDiffuseLoc, 0);
+		glUniform1i(matSpecularLoc, 1);
+		glUniform1f(matShineLoc, 32.0f);
+
+		// Set lights properties
+		GLint lightAmbientLoc = lightingShader.getUniformLocation("light.ambient");
+		GLint lightDiffuseLoc = lightingShader.getUniformLocation("light.diffuse");
+		GLint lightSpecularLoc = lightingShader.getUniformLocation("light.specular");
+		GLint lightPosLoc = lightingShader.getUniformLocation("light.position");
+		glUniform3f(lightAmbientLoc, 0.2f, 0.2f, 0.2f);
+		glUniform3f(lightDiffuseLoc, 0.9f, 0.9f, 0.9f);
+		glUniform3f(lightSpecularLoc, 1.0f, 1.0f, 1.0f);
+		glUniform3f(lightPosLoc, lightPos.x, lightPos.y, lightPos.z);
+
+		//Fin codigo luz
+
+		//Inicio codigo de modelos
+
+		shader.turnOn();
+
+		// Get the uniform locations
+		modelLoc = shader.getUniformLocation("model");
+		viewLoc = shader.getUniformLocation("view");
+		projLoc = shader.getUniformLocation("projection");
+
+		// Pass the matrices to the shader
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+		// Set material properties
+		// matDiffuseLoc = lightingShader.getUniformLocation("material.diffuse");
+		// matSpecularLoc = lightingShader.getUniformLocation("material.specular");
+		matShineLoc = lightingShader.getUniformLocation("material.shininess");
+		// glUniform1i(matDiffuseLoc, 0);
+		// glUniform1i(matSpecularLoc, 1);
+		glUniform1f(matShineLoc, 32.0f);
+
+		// Set lights properties
+		lightAmbientLoc = lightingShader.getUniformLocation("light.ambient");
+		lightDiffuseLoc = lightingShader.getUniformLocation("light.diffuse");
+		lightSpecularLoc = lightingShader.getUniformLocation("light.specular");
+		lightPosLoc = lightingShader.getUniformLocation("light.position");
+		glUniform3f(lightAmbientLoc, 0.2f, 0.2f, 0.2f);
+		glUniform3f(lightDiffuseLoc, 0.9f, 0.9f, 0.9f);
+		glUniform3f(lightSpecularLoc, 1.0f, 1.0f, 1.0f);
+		glUniform3f(lightPosLoc, lightPos.x, lightPos.y, lightPos.z);
+
 
 		// Draw the loaded model
 		glm::mat4 model;
@@ -79,7 +162,7 @@ void GLApplication::applicationLoop() {
 
 		// Draw the loaded model
 		glm::mat4 model1;
-		model1 = glm::translate(model1, glm::vec3(1.5f, -1.75f, 0.0f));
+		model1 = glm::translate(model1, glm::vec3(0.75f, -1.75f, 0.0f));
 		model1 = glm::scale(model1, glm::vec3(0.2f, 0.2f, 0.2f));
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model1));
 
@@ -87,13 +170,33 @@ void GLApplication::applicationLoop() {
 
 		// Draw the loaded model
 		glm::mat4 model2;
-		model2 = glm::translate(model2, glm::vec3(0.75f, -1.75f, 0.0f));
+		model2 = glm::translate(model2, glm::vec3(-0.75f, -1.75f, 0.0f));
 		model2 = glm::scale(model2, glm::vec3(0.2f, 0.2f, 0.2f));
 		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model2));
 
 		objModel2.render(&shader);
 
 		shader.turnOff();
+	//Fin dibujo modelos
+
+	//Dibujo esfera iluminacion
+		lampShader.turnOn();
+
+		// Create transformations
+		modelLoc = lampShader.getUniformLocation("model");
+		viewLoc = lampShader.getUniformLocation("view");
+		projLoc = lampShader.getUniformLocation("projection");
+		// Pass the matrices to the shader
+		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+		model = glm::scale(glm::mat4(), glm::vec3(0.5, 0.5, 0.5));
+		model = glm::translate(model, lightPos);
+		model = glm::scale(model, glm::vec3(0.4, 0.4, 0.4));
+		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+		sp.render();
+		lampShader.turnOff();
+	
 
 		windowManager->swapTheBuffers();
 	}
--- P2_E4/Shaders/lightingSpecularMap.fs
+++ P2_E4/Shaders/lightingSpecularMap.fs
@@ -1,5 +1,7 @@
 #version 330 core
 struct Material {
+    sampler2D diffuse;
+    sampler2D specular;
     float shininess;
 };
 
@@ -18,7 +20,6 @@ in vec2 TexCoords;
   
 out vec4 color;
 
-uniform sampler2D texture_diffuse1;
 uniform vec3 viewPos;
 uniform Light light;  
 uniform Material material;
@@ -26,22 +27,20 @@ uniform Material material;
 void main()
 {
     // Ambient
-    vec3 ambient = light.ambient * vec3(texture(texture_diffuse1, TexCoords));
+    vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
   	
     // Diffuse 
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(light.position - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = light.diffuse * diff * vec3(texture(texture_diffuse1, TexCoords));
+    vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
     
     // Specular
     float specularStrength = 0.5f;
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular = light.specular * (spec * vec3(texture(texture_diffuse1, TexCoords)));
+    vec3 specular = light.specular * (spec * vec3(texture(material.specular, TexCoords)));
     
-    //color = vec4(ambient + diffuse + specular, 1.0f);
-    color = vec4(ambient + diffuse, 1.0f);
-}
- 
+    color = vec4(ambient + diffuse + specular, 1.0f);
+} 
